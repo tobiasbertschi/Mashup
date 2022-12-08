caf7f3751d77cdf5b04cf623d2291e771019a256
tobiasbertschi <gotteron89tb@gmail.com>
2022-12-08 10:56:12 +0100

    daily commit

--- Mashup-Backend/src/main/java/ch/bbw/mashupbackend/controller/ApiController.java
+++ Mashup-Backend/src/main/java/ch/bbw/mashupbackend/controller/ApiController.java
@@ -1,30 +1,31 @@
 package ch.bbw.mashupbackend.controller;
 
+import ch.bbw.mashupbackend.service.CatService;
+import ch.bbw.mashupbackend.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 @RestController
 public class ApiController {
 
-    private String openWeatherKey = "3eb44406d378ea59d127e5daa17b4bee";
-    private String request = "https://api.openweathermap.org/data/2.5/weather?q=Zuerich&appid=" + openWeatherKey;
+    @Autowired
+    final WeatherService weatherService;
+    final CatService catService;
+
+    public ApiController(WeatherService weatherService, CatService catService) {
+        this.weatherService = weatherService;
+        this.catService = catService;
+    }
 
     @GetMapping("/weather")
     public String getWeather() {
+        return weatherService.getWeather();
+    }
 
-        try {
-            URL url = new URL(request);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            //return con.ge;
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        return "fail";
+    @GetMapping("/cats")
+    public String getCats() {
+        return catService.getCat();
     }
 
 }
--- Mashup-Backend/src/main/java/ch/bbw/mashupbackend/service/HoroscopeService.java
+++ Mashup-Backend/src/main/java/ch/bbw/mashupbackend/service/HoroscopeService.java
@@ -1,7 +1,6 @@
 package ch.bbw.mashupbackend.service;
 
-public class ApiService {
-
+public class HoroscopeService {
 
 
 }
