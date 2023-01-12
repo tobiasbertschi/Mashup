2d40f31a115aaa0c56328d3b95c69f6f82118ec0
Yves Huber <yves.huber@lernende.bbw.ch>
2023-01-12 11:02:15 +0100

    styling

--- frontend/src/Mainpage.js
+++ frontend/src/Mainpage.js
@@ -50,17 +50,21 @@ export default function Mainpage() {
       <header className="App-header">
         <Grid container direction="column" justifyContent="center" alignItems="strech">
           <Grid container direction="row" alignItems="center">
+            <Grid xs={12}>
+              <h1>Bored today?</h1>
+
+            </Grid>
             <Grid xs={6} className="weather">
               <h2>Weather</h2>
-              <h2>{Math.round(weather?.main?.temp - 273)}°C</h2>
-              <h2>Location: {weather?.name}</h2>
-              <h2>Current Weather: {weather?.weather[0]?.main}</h2>
-              <h2>Wind: {Math.round(weather?.wind?.speed)}km/h</h2>
+              <h3>{Math.round(weather?.main?.temp - 273)}°C</h3>
+              <h3>Location: {weather?.name}</h3>
+              <h3>Current Weather: {weather?.weather[0]?.main}</h3>
+              <h3>Wind: {Math.round(weather?.wind?.speed)}km/h</h3>
             </Grid>
             <Grid xs={6}>
               <h2>Cat</h2>
               {catsrc !== undefined
-              ? <img id="image" src={URL.createObjectURL(catsrc)} width="200" height="200"/>
+              ? <img id="image" src={URL.createObjectURL(catsrc)} width="150" height="150"/>
               :<></>}
             </Grid>
           </Grid>
@@ -68,16 +72,16 @@ export default function Mainpage() {
           <Grid container direction="row" alignItems="center">
             <Grid xs={12}>
               <h2>Facts</h2>
-              <h2> {joke}</h2>
+              <h3> {joke}</h3>
             </Grid>
            
           </Grid>
           <Grid xs={12}  className="horoscope">
             <h2>Horoscope</h2>
-            <h2>{horoscope?.description}</h2>
-            <h2>lucky time: {horoscope?.lucky_time}</h2>
-            <h2>lucky number: {horoscope?.lucky_number}</h2>
-            <h2>mood: {horoscope?.mood}</h2>
+            <h3>{horoscope?.description}</h3>
+            <h3>lucky time: {horoscope?.lucky_time}</h3>
+            <h3>lucky number: {horoscope?.lucky_number}</h3>
+            <h3>mood: {horoscope?.mood}</h3>
           </Grid>
         </Grid>
       </header>
